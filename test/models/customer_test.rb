b203f392bbc7def1f83590fe936d39f8bfb49cf6
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-14 15:34:32 -0700

    added basic customer tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,19 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
+  let(:customer) { customers(:one) }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  it "can be created" do
+    expect(customer.valid?).must_equal true
+  end
+
+  it "requires name, registered_at, address, city, state, postal_code, phone" do
+    required_fields = [:name, :registered_at, :address, :city, :state, :postal_code, :phone]
+
+    required_fields.each do |field|
+      customer[field] = nil
+      expect(customer.valid?).must_equal false
+      customer.reload
+    end
   end
 end
