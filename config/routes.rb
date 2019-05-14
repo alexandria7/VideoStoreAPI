0dcc41bbaf7c52720c91ae9f5be3d4b4513122f6
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-14 14:14:57 -0700

    routes for Movies controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :movies, only: [:index, :show, :create]
+  # get "movies/index"
+  # get "movies/show"
+  # get "movies/create"
+  
+
+  get "movies/zomg"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
