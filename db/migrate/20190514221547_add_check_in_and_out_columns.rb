b20b62a928fdd62001ef4d862b5822b89345269b
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-14 15:44:41 -0700

    deleted down migration file until we know how we want to set up rental model

--- db/migrate/20190514221547_add_check_in_and_out_columns.rb
+++
@@ -1,6 +0,0 @@
-class AddCheckInAndOutColumns < ActiveRecord::Migration[5.2]
-  def change
-    add_column :check_out, :date, default: Date.today
-    add_column :return_date, :date, default: (Date.today + 7)
-  end
-end
