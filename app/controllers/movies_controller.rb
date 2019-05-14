af417bc432121ace736733d16a5a8006489926db
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-14 16:41:20 -0700

    movies index action and tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,8 @@
 class MoviesController < ApplicationController
   def index
+    movies = Movie.all
+    
+    render json: movies.as_json(only: [:id, :title, :release_date]), status: :ok
   end
 
   def show
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,19 +1,49 @@
 require "test_helper"
 
 describe MoviesController do
-  it "should get index" do
-    get movies_index_url
-    value(response).must_be :success?
-  end
+  describe "index" do
+    it "should get index" do
+      get movies_path
+      must_respond_with :success
+    end
 
-  it "should get show" do
-    get movies_show_url
-    value(response).must_be :success?
-  end
+    it "returns json" do
+      get movies_path
+      expect(response.header["Content-Type"]).must_include "json"
+    end
 
-  it "should get create" do
-    get movies_create_url
-    value(response).must_be :success?
+    it "returns an Array" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.must_be_kind_of Array
+    end
+
+    it "returns all of the movies" do
+      get movies_path
+
+      body = JSON.parse(response.body)
+      body.length.must_equal Movie.count
+    end
+
+    it "returns movies with exactly the required fields" do
+      keys = %w(id release_date title)
+
+      get movies_path
+      body = JSON.parse(response.body)
+      body.each do |movie|
+        movie.keys.sort.must_equal keys
+      end
+    end
   end
+  # it "should get show" do
+  #   get movies_show_url
+  #   value(response).must_be :success?
+  # end
+
+  # it "should get create" do
+  #   get movies_create_url
+  #   value(response).must_be :success?
+  # end
 
 end
