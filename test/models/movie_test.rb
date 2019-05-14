e92e0009389e17eced5b7d9148c939c8b842ddcf
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-14 14:52:00 -0700

    added tests for movie validations

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -1,9 +1,29 @@
 require "test_helper"
 
 describe Movie do
-  let(:movie) { Movie.new }
+  let(:movie) { movies(:one) }
 
-  it "must be valid" do
-    value(movie).must_be :valid?
+  it "can be created" do
+    expect(movie.valid?).must_equal true
+  end
+
+  it "requires title, overview, release_date, and inventory" do
+    required_fields = [:title, :overview, :release_date, :inventory]
+
+    required_fields.each do |field|
+      movie[field] = nil
+      expect(movie.valid?).must_equal false
+      movie.reload
+    end
+  end
+
+  it "accepts a numeric inventory value" do
+    movie.inventory = 2
+    expect(movie.valid?).must_equal true
+  end
+
+  it "does not accept a non-numeric inventory value" do 
+    movie.inventory = "seven"
+    expect(movie.valid?).must_equal false
   end
 end
