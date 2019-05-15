63a2e3a01fa28a51ed8c7f7c27481787cf3bfbc1
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-15 09:59:20 -0700

    changed back json returns for index

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,7 +2,7 @@ class MoviesController < ApplicationController
   def index
     movies = Movie.all
 
-    render json: movies.as_json(only: [:id, :title, :overview, :release_date, :inventory]), status: :ok
+    render json: movies.as_json(only: [:id, :title, :release_date]), status: :ok
   end
 
   def show
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -28,7 +28,7 @@ describe MoviesController do
     end
 
     it "returns movies with exactly the required fields" do
-      keys = %w(id inventory overview release_date title)
+      keys = %w(id release_date title)
 
       get movies_path
       body = JSON.parse(response.body)
