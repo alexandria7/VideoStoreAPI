9715a7ffea7608ea644cad7aa5a6f582a0c67037
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-15 13:05:36 -0700

    added available_inventory method and added it to show method

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -7,23 +7,25 @@ describe Movie do
     expect(movie.valid?).must_equal true
   end
 
-  it "requires title, overview, release_date, and inventory" do
-    required_fields = [:title, :overview, :release_date, :inventory]
+  describe "validations" do
+    it "requires title, overview, release_date, and inventory" do
+      required_fields = [:title, :overview, :release_date, :inventory]
 
-    required_fields.each do |field|
-      movie[field] = nil
-      expect(movie.valid?).must_equal false
-      movie.reload
+      required_fields.each do |field|
+        movie[field] = nil
+        expect(movie.valid?).must_equal false
+        movie.reload
+      end
     end
-  end
 
-  it "accepts a numeric inventory value" do
-    movie.inventory = 2
-    expect(movie.valid?).must_equal true
-  end
+    it "accepts a numeric inventory value" do
+      movie.inventory = 2
+      expect(movie.valid?).must_equal true
+    end
 
-  it "does not accept a non-numeric inventory value" do 
-    movie.inventory = "seven"
-    expect(movie.valid?).must_equal false
+    it "does not accept a non-numeric inventory value" do
+      movie.inventory = "seven"
+      expect(movie.valid?).must_equal false
+    end
   end
 end
