a46c38d8e7c1151fd134d5c0518ac060367c51dc
Bitaman <b.amani.s@gmail.com>
2019-05-14 17:43:27 -0700

    added the actions to the customer controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,40 @@
 class CustomersController < ApplicationController
+    def index
+        customer = Customer.all  
+        render json: customer.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone]), status: :ok
+    end
+
+    def show
+        @customer = Customer.find_by(id: params[:id])
+        if @customer.nil?
+            render json: { errors: {
+                title: ["Customer #{params[:id]} not found"]
+            }},
+            status: :not_found     
+        else
+            render json: @customer.as_json(only: [:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone]), status: :ok
+                
+        end
+    end
+
+    def create
+        customer = Customer.new(customer_params)
+        if customer.save
+            render json: { id: customer.id }, status: :ok
+        else
+            render json: {
+                errors: {
+                    title: ["could not create '#{customer_params[:name]}' Customer"]
+                },
+                message: customer.error.messages
+            }, status: :bad_request
+        end  
+    end
+    
+    private
+    
+    def customer_params
+        params.permit(:id, :name, :registered_at, :address, :city, :state, :postal_code, :phone)
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # get "movies/index"
   # get "movies/show"
   # get "movies/create"
+  resources :customer, only: [:index, :show, :create]
   
 
   get "movies/zomg"
