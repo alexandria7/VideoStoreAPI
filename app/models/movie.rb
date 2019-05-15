eabe72c03838c8fd33baf67ef405d43a769f2993
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-15 12:54:55 -0700

    added available_inventory model method

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,4 +3,8 @@ class Movie < ApplicationRecord
   validates :overview, presence: true
   validates :release_date, presence: true
   validates :inventory, presence: true, numericality: true
+
+  def available_inventory
+    return self.inventory
+  end
 end
