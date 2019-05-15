6dce3f966ec701b460840472c2e821c427243799
Bitaman <b.amani.s@gmail.com>
2019-05-15 11:57:51 -0700

    added relationship tests for rental, all passing

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -6,4 +6,25 @@ describe Rental do
   it "must be valid" do
     value(rental).must_be :valid?
   end
+  describe "relations" do
+    it "can have a customer" do
+      rental.customer.must_equal customers(:one)
+    end
+    it "can set a customer" do
+      customer = customers(:one)
+      rental = Rental.new(customer_id: customer.id, movie_id: 3 )
+
+      rental.customer.id.must_equal customer.id
+    end
+    it "can have a movie" do
+      rental.movie.must_equal movies(:one)
+    end
+    it "can set a movie" do
+      movie = movies(:one)
+      rental = Rental.new(customer_id: 5, movie_id: movie.id )
+
+      rental.movie.id.must_equal movie.id
+    end
+
+  end
 end
