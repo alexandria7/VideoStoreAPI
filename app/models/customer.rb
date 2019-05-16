095989bf2e121e8628ba23ecefb5e43a711cef03
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-16 09:39:35 -0700

    removed movies_checked_out_count method from customer model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,6 @@
 class Customer < ApplicationRecord
   validates :name, presence: true
-  
+
   has_many :rentals
   has_many :movies, through: :rentals
-  def movies_checked_out_count
-    return 0
-  end
 end
