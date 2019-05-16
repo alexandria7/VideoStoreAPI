0b9c50e44ad497537b1a416d6dc193064df8a032
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-16 12:58:07 -0700

    wrote tests for checkout method

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,7 +1,33 @@
 class RentalsController < ApplicationController
   def check_out
+    rental = Rental.new(rental_params)
+
+    if rental.movie.available_inventory > 0
+      
+      if rental.save
+        rental.movie.available_inventory -= 1
+        rental.movie.save
+
+        rental.customer.movies_checked_out_count += 1
+        rental.customer.save
+        rental.check_out = rental.created_at
+        rental.due_date = rental.created_at + 7.days
+        render json: rental.as_json(only: [:id, :customer_id, :movie_id, :check_out, :due_date]), status: :ok
+      else
+        render json: {ok: false, errors: rental.errors.messages},
+               status: :bad_request
+      end
+    else
+      render json: {ok: false, error_message: "No available inventory"}, status: :bad_request
+    end
   end
 
   def check_in
+  end
+
+  private
+
+  def rental_params
+    params.permit(:customer_id, :movie_id)
   end
 end
--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -1,7 +1,52 @@
 require "test_helper"
 
 describe RentalsController do
-  # it "must be a real test" do
-  #   flunk "Need real tests"
-  # end
+  let(:customer) { customers(:two) }
+  let(:movie) { movies(:one) }
+  describe "check-out" do
+    let(:rental_data) {
+      {
+        customer_id: customer.id,
+        movie_id: movie.id,
+      }
+    }
+    it "should create a new rental given correct information" do
+      movie_inventory = movie.available_inventory
+      customer_checked_out_count = customer.movies_checked_out_count
+
+      expect {
+        post check_out_path, params: rental_data
+      }.must_change "Rental.count", 1
+
+      rental = Rental.find_by(customer_id: customer.id, movie_id: movie.id)
+
+      expect(rental.movie.available_inventory).must_equal movie_inventory - 1
+      expect(rental.customer.movies_checked_out_count).must_equal customer_checked_out_count + 1
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "id"
+
+      json_rental = Rental.find(body["id"].to_i)
+
+      expect(json_rental.customer_id).must_equal rental_data[:customer_id]
+      expect(json_rental.movie_id).must_equal rental_data[:movie_id]
+      must_respond_with :success
+    end
+
+    it "should send an error message if given bad information" do
+      rental_data[:customer_id] = nil
+
+      expect {
+        post check_out_path, params: rental_data
+      }.wont_change "Rental.count"
+
+      body = JSON.parse(response.body)
+
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "customer"
+      must_respond_with :bad_request
+    end
+  end
 end
