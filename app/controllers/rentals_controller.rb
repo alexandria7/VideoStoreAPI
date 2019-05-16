f3f312c946f64da2352ddc95dc21aed7bdf928c9
Alexandria McCarthy <alexandria.mccarthy77@gmail.com>
2019-05-16 09:41:31 -0700

    created rentals controller and added routes

---
+++ app/controllers/rentals_controller.rb
@@ -0,0 +1,7 @@
+class RentalsController < ApplicationController
+  def check_out
+  end
+
+  def check_in
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # get "movies/create"
   resources :customers, only: [:index, :show, :create]
 
+  post "/rentals/check-out", to: "rentals#check_out", as: "check_out"
+  post "/rentals/check-in", to: "rentals#check_in", as: "check_in"
+
   # get "movies/zomg"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
